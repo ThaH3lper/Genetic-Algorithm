64f5cc0ffd3a1464a56897134473a245823fa74e
Patrik Nilsson <craftopia@live.com>
2016-02-22 09:37:08 +0100

    init

---
+++ Genetic Algorithm/Genetic Algorithm/Game1.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Genetic_Algorithm
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void UnloadContent()
+        {
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            KeyMouseReader.Update();
+            if (KeyMouseReader.KeyClick(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Genetic Algorithm/Genetic Algorithm/KeyMouseReader.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+
+static class KeyMouseReader
+{
+	public static KeyboardState keyState, oldKeyState = Keyboard.GetState();
+	public static MouseState mouseState, oldMouseState = Mouse.GetState();
+
+	public static bool KeyClick(Keys key) {
+		return keyState.IsKeyDown(key) && oldKeyState.IsKeyUp(key);
+	}
+    public static bool KeyPress(Keys key){
+        return keyState.IsKeyDown(key);
+    }
+    public static List<Keys> GetKeyPressed(){
+        List<Keys> keys = new List<Keys>();
+        foreach(Keys key in keyState.GetPressedKeys())
+        {
+            if(oldKeyState.IsKeyUp(key))
+                keys.Add(key);
+        }
+        return keys;
+    }
+	public static bool LeftClick() {
+		return mouseState.LeftButton == ButtonState.Pressed && oldMouseState.LeftButton == ButtonState.Released;
+	}
+    public static bool LeftPressed(){
+        return mouseState.LeftButton == ButtonState.Pressed;
+    }
+	public static bool RightClick() {
+		return mouseState.RightButton == ButtonState.Pressed && oldMouseState.RightButton == ButtonState.Released;
+	}
+    public static bool RightPressed(){
+        return mouseState.RightButton == ButtonState.Pressed;
+    }
+    public static Vector2 GetMousePos(){
+        return new Vector2((int)mouseState.X, (int)mouseState.Y);
+    }
+
+	public static void Update() {
+		oldKeyState = keyState;
+		keyState = Keyboard.GetState();
+		oldMouseState = mouseState;
+		mouseState = Mouse.GetState();
+	}
+}
